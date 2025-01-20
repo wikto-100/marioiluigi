baacc53b2ff0b4bfee41a2e5f6b7b52e25208c56
mpoprawa <94672269+mpoprawa@users.noreply.github.com>
2025-01-20 17:57:58 +0100

    Delete frontend directory

--- frontend/PlaceholderBot.py
+++
@@ -1,7 +0,0 @@
-TEST_COUNTER = 0
-def get_bot_move(board_state):
-    if board_state == 'rnbqkbnr/pppppppp/8/8/8/5P2/PPPPP1PP/RNBQKBNR':
-        move = "7e5e"
-    else:
-        move = "8d4h"
-    return move
